5ddb436d1dee976dbeaa0a8e05d14fb97bbe5302
virat02 <virat02@ccs.neu.edu>
2019-03-17 00:10:39 -0400

    working on pagerank scala

--- Pagerank Scala/src/main/scala/pr/Pagerank.scala
+++ Pagerank Scala/src/main/scala/pr/Pagerank.scala
@@ -16,60 +16,83 @@ object Pagerank {
   def pow2(v: Int)= v*v
 
   //Make the row of our graph based on the input parameter k
-  def makeRow(x: Int, k: Int): Array[String] = {
+  def makeRow(x: Int, k: Int, sc: SparkContext):  RDD[(Int,Int)] = {
     var i = x
 
-    val sb = StringBuilder.newBuilder
-
+    var row: List[(Int, Int)] = List()
     while(i <= k) {
-      sb.append(i.toString)
-      if(i != k){
-        sb.append(",")
+
+      if(i == k){
+        row = row:+(i,0)
+      }
+      else {
+        row = row:+(i, i+1)
       }
       i+=1
     }
 
-    val str : Array[String] = sb.toString().split(",")
+    val s = sc.parallelize(row)
 
-    str
+    s
   }
 
   //Make the input based on the input parameter k and store it in input file input.csv
-  def makeInput(k: String, input: String) {
-    val out = new BufferedWriter(new FileWriter(input+"/input.csv"))
-    val writer = new CSVWriter(out)
-    val x = k.toInt
+  def makeGraph(k: Int, input: String, sc: SparkContext):  RDD[(Int,Int)] = {
 
-    val listOfRecords = new java.util.ArrayList[Array[String]]()
+    var listOfRecords = sc.emptyRDD[(Int, Int)]
 
-    for(i <- 1 to pow2(x) by x) {
-      listOfRecords.add(makeRow(i, i+x-1))
+    for(i <- 1 to pow2(k) by k) {
+      listOfRecords = listOfRecords union makeRow(i, i+k-1, sc)
     }
 
-    writer.writeAll(listOfRecords)
-    out.close()
-  }
-
-  //Make the graph based on the input
-  def makeGraph(input: RDD[String]): Unit = {
-    
+    listOfRecords
   }
   
   def main(args: Array[String]) {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
     if (args.length != 3) {
-      logger.error("Usage:\nfc.FollowerCountMain <input dir> <output dir> <param-k>")
+      logger.error("Usage:\npr.Pagerank <input dir> <output dir> <param-k>")
       System.exit(1)
     }
     val conf = new SparkConf().setAppName("Pagerank")
     val sc = new SparkContext(conf)
 
-    makeInput(args{2}, args{0})
+    val param = args{2}.toInt
+    val graphRDD = makeGraph(param, args{0}, sc)
 
-    //Read input file
-    val textFile = sc.textFile(args(0))
+    //persist graphRDD
+    graphRDD.persist()
 
-    makeGraph(textFile)
+    val zeroTuple = (0,0f)
+    val zeroRow: List[(Int, Float)] = List()
+    val dummyNodeRDD = sc.parallelize(zeroRow:+zeroTuple)
+
+    val rankRDD = graphRDD.map(x =>  (x._1,1f/pow2(param)))
+
+    val finalRankRDD = rankRDD union dummyNodeRDD
+
+    //persist finalRankRDD
+    finalRankRDD.persist()
+
+//    println(finalRankRDD.collect().foreach(println))
+//    println(graphRDD.collect().foreach(println))
+
+    val contributions = graphRDD.join(finalRankRDD)
+        .map(x => if (x._1 != 0) (x._1, x._2) else ())
+//      .flatMap{ case(pagename,(links,rank)) =>
+////      // if it is a dangling node output an empty list
+////      if(links==0){
+////        List()}
+////      else{
+////        // else we map the contribution to the outgoing node
+////        //links.map(dest=>(dest,rank/size))
+////      }}
+////      // sum all the contributions from the node
+////      .reduceByKey((x,y)=>(x+y))
+
+    //val ranks = contributions.reduceByKey((x, y) => x + y).mapValues(v => 0.15 + 0.85*v)
+
+    println(contributions.collect().foreach(println))
 
 //
 //    //fetch the user_id_b, from a given input as user_id_a,user_id_b
